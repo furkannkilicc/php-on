f8a8d44460b5d019ccb3c7cc4a5f238e8e3dd7d1
Furkan Kilic <kilic.furkank@gmail.com>
2022-06-25 01:09:13 +0300

    php dersleri day1

---
+++ first.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1> html içi</h1>
+<script>
+    alert('js kodu php içinde  ');
+</script>
+</body>
+</html>
+
+
+
+
+<h1> "selam canım "</h1>
+<?php
+echo  'furkan';
+print 'deneme print ';
+//asdasd
+# asda
+/*
+adasdasda
+asdasdas
+adsasda*/
+
+
+?>
+<h1> <?php  echo'html içi php kullanımı'?> </h1>
+<h2> <?='kısa kullanum echo ' ?> </h2>
+
+
+
+<style>
+    h1{
+        color: green;
+    }
+</style>
